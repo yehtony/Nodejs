e2ed976bafcc4366e58f7b028ead4c1127507bec
yexuan <yexuanncu@gmail.com>
2022-06-24 23:22:51 +0800

    add JSON

--- api/dist/app.dev.js
+++ api/dist/app.dev.js
@@ -1,47 +1,52 @@
+"use strict";
+
 var createError = require('http-errors');
+
 var express = require('express');
+
 var path = require('path');
+
 var cookieParser = require('cookie-parser');
+
 var logger = require('morgan');
+
 var cors = require("cors");
 
 var indexRouter = require('./routes/index');
+
 var usersRouter = require('./routes/users');
+
 var testAPIRouter = require("./routes/testAPI");
+
 var jsonRouter = require("./routes/json");
 
-var app = express();
+var app = express(); // view engine setup
 
-// view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
-
 app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({
+  extended: false
+}));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(express["static"](path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use("/testAPI", testAPIRouter);
-app.use("/json", jsonRouter);
+app.use("/json", jsonRouter); // catch 404 and forward to error handler
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
-});
+}); // error handler
 
-// error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get('env') === 'development' ? err : {}; // render the error page
 
-  // render the error page
   res.status(err.status || 500);
   res.render('error');
 });
-
 module.exports = app;
